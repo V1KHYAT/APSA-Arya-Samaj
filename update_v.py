3b875272136eb6764b549f10000bc37ecfd13575
Vikhyat Kaushik <vikhyatkaushik@users.noreply.github.com>
2026-08-19 12:11:17 +0530

    Fix image paths, make dropdowns equal width, add image to Contact Us

--- update_v.py
+++ update_v.py
@@ -1,8 +1,7 @@
 import os, re
-html_files = [f for f in os.listdir('.') if f.endswith('.html')]
-for file in html_files:
+for file in [f for f in os.listdir('.') if f.endswith('.html')]:
     with open(file, 'r', encoding='utf-8') as f:
         content = f.read()
-    content = re.sub(r'href="css/style\.css\?v=\d+"', 'href="css/style.css?v=4"', content)
+    content = re.sub(r'href="css/style\.css\?v=\d+"', 'href="css/style.css?v=5"', content)
     with open(file, 'w', encoding='utf-8') as f:
         f.write(content)
