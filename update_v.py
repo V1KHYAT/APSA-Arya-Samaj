0358c33731b4f134cd8a2af3391c495b3da5ee5f
Vikhyat Kaushik <vikhyatkaushik@users.noreply.github.com>
2026-08-19 12:48:54 +0530

    Implement exact editorial mega menu and dropdowns matching design reference

--- update_v.py
+++
@@ -1,7 +0,0 @@
-import os, re
-for file in [f for f in os.listdir('.') if f.endswith('.html')]:
-    with open(file, 'r', encoding='utf-8') as f:
-        content = f.read()
-    content = re.sub(r'href="css/style\.css\?v=\d+"', 'href="css/style.css?v=5"', content)
-    with open(file, 'w', encoding='utf-8') as f:
-        f.write(content)
